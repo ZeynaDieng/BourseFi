f69d6ea5140c5f1e415e001f91687b6bcd1c5302
ZeynaDieng <zeynash1@gmail.com>
2026-08-22 12:53:24 +0000

    fix: Remove duplicate mobile bottom nav bar from student-app layout

--- app/layouts/student-app.vue
+++ app/layouts/student-app.vue
@@ -1,16 +1,15 @@
 <template>
-  <div class="font-body">
+  <div class="font-body min-h-screen bg-slate-50">
     <StudentTopBar />
     <div class="mx-auto flex w-full max-w-6xl">
       <StudentSidebar />
       <div
         id="contenu-principal"
-        class="min-w-0 flex-1 pb-28 md:pb-8"
+        class="min-w-0 flex-1 pb-8 md:pb-8"
         tabindex="-1"
       >
         <slot />
       </div>
     </div>
-    <StudentMobileNav />
   </div>
 </template>
